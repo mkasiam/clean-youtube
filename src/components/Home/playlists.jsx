a40e891df3483cef14d08cac796a7e077749bcef
Kawsar Ahmmed <kawsarahmmed10001@gmail.com>
2026-05-19 00:05:07 +0600

    feat: reverse playlist display order and update grid layout to three columns

--- src/components/Home/playlists.jsx
+++ src/components/Home/playlists.jsx
@@ -19,7 +19,7 @@ const Playlists = ({
           }}
         >
           {playlistArrays?.map((playlist) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={playlist.playlistId}>
+            <Grid item xs={12} sm={6} md={4} lg={4} key={playlist.playlistId}>
               <PlaylistCardItem
                 playlistId={playlist.playlistId}
                 playlistThumbnail={playlist.playlistThumbnail}
